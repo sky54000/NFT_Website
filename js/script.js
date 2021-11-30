61fc95eb739cc1d0d0ea19b1492e2d208fafd0f2
Robin Nassoy <robleboss@gmail.com>
2021-11-30 23:05:09 +0100

    Basic_version

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	
+    var headerHeight = $('header').outerHeight(); // Target your header navigation here
+      
+    $('.menu_header a').click(function(e) {
+        
+        var targetHref   = $(this).attr('href');
+        
+      $('html, body').animate({
+          scrollTop: $(targetHref).offset().top - headerHeight // Add it to the calculation here
+      }, 1000);
+      
+      e.preventDefault();
+    });
+  });
